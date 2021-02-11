e75d504712401b9be87cca6b7cc6c403bfbf7624
Elle Kline <ellankline@gmail.com>
2021-02-11 10:13:41 -0800

    first commit, added text and modified css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const displayStyle = {
+    fontFamily: "open sans",
+    align: "left",
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <h1>Hello Dojo!</h1>
+  <h3>Things I need to do:</h3>
+  <div>
+    <ul>
+      <li>learn React</li>
+      <li>climb Mt. Everest</li>
+      <li>run a marathon</li>
+      <li>feed the dog</li>
+    </ul>
+  </div>
     </div>
   );
 }
